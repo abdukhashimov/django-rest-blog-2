b8b9980fce8f670810f993cadd9f9b7cf273b2b0
Madiyor <abdukhashimov@yandex.ru>
2020-02-08 17:17:41 +0500

    user create is working

--- user/models.py
+++ user/models.py
@@ -8,13 +8,13 @@ from django.contrib.auth.models import (
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_args):
         """Creates and saves the user with the given email, and password"""
         if not email:
             raise ValueError('Users must have an emaild address!')
 
         user = self.model(
-            email=self.normalize_email(email))
+            email=self.normalize_email(email), **extra_args)
         user.set_password(password)
         user.save(using=self._db)
         return user
