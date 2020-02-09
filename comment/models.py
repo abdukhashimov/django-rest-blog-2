73b93830b98c95f043a3f85f8b1855ac25abc119
Madiyor <abdukhashimov@yandex.ru>
2020-02-09 19:39:00 +0500

    Added comments

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class CommentManager(models.Manager):
+    def get_parent_comment(self, id):
+        return super(
+            CommentManager, self
+        ).get_queryset().filter(id=id, parent_id__isnull=True)
+
+    def get_child_comment(self, id):
+        parents = Comment.objects.filter(id=id)
+        collector = NestedObjects(using='default')
+        collector.collect(parents)
+        collector.data[parents[0].__class__].remove(parents[0])
+        return collector.data[parents[0].__class__]
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent = models.ForeignKey(
+        'self', related_name='reply', null=True, blank=True,
+        on_delete=models.CASCADE
+    )
+
+    objects = CommentManager()
+
+    def __str__(self):
+        if self.parent is None:
+            return "{}'s comment".format(str(self.author))
+        return "{}'s reply".format(str(self.author))
+
+    @property
+    def parent_comment(self):
+        return Comment.objects.get_parent_comment(self.id)
+
+    @property
+    def child_comments(self):
+        return Comment.objects.get_child_comment(self.id)
