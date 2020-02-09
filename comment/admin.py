e61df75e60aed36e2ad0c6474c551e490b7c5110
Madiyor <abdukhashimov@yandex.ru>
2020-02-09 21:40:10 +0500

    added CommentAdmin

--- comment/admin.py
+++ comment/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from comment.models import Comment
 
 
-admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('comment', 'parent', 'post')
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        return super().save_model(request, obj, form, change)
+
+
+admin.site.register(Comment, CommentAdmin)
